42745f52022b97f05a95c7b74cfa56812a2c2872
anpigon <markan82@gmail.com>
2024-01-30 14:42:23 +0900

    Create Comments.tsx

---
+++ quartz/components/Comments.tsx
@@ -0,0 +1,26 @@
+import { QuartzComponentConstructor } from "./types"
+
+export default (() => {
+  function Footer() {
+    return (
+      <script
+        src="https://giscus.app/client.js"
+        data-repo="anpigon/quartz"
+        data-repo-id="R_kgDOLKhz-Q"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOLKhz-c4CczBZ"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossOrigin="anonymous"
+        async
+      ></script>
+    )
+  }
+
+  return Footer
+}) satisfies QuartzComponentConstructor
